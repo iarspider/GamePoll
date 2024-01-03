4b8fd162b7101bf15c2473fe2c02aa125d9edcd1
iarspider <iarspider@gmail.com>
2024-01-03 13:48:54 +0100

    Fix

--- polls/admin.py
+++ polls/admin.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 from .models import Game, Tag, Poll, PollBlock, Vote
 
 # Register your models here.
-admin.register(Game)
-admin.register(Tag)
-admin.register(Poll)
-admin.register(PollBlock)
-admin.register(Vote)
+admin.site.register(Game)
+admin.site.register(Tag)
+admin.site.register(Poll)
+admin.site.register(PollBlock)
+admin.site.register(Vote)
