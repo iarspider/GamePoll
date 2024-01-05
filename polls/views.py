15b1ff0af0c74e9a94c463bd84e91460ea5d0ffb
Veniamin Arefev <veniamin.arefev@mail.ru>
2024-01-05 16:45:09 +0300

    Added few margins to layout.html (#3)
    * Added few margins to layout.html
    Auto reformatted all python with black and html with Pycharm

--- polls/views.py
+++ polls/views.py
@@ -37,10 +37,13 @@ def poll_list(request):
 
 
 def login_view(request):
-    return render(
-        request,
-        "polls/login.html",
-    )
+    if not request.user.is_authenticated:
+        return render(
+            request,
+            "polls/login.html",
+        )
+    else:
+        return redirect("profile")
 
 
 @login_required
